b9c1ca12bdd08d46f2beae70cfaf44c2f717bf34
Yogesh Giri <yogeshgiri904@gmail.com>
2021-09-27 13:37:03 +0530

    init

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Shri Ram Org | Official</title>
+    <link rel="stylesheet" href="header.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <nav>
+        <div class="main">
+            <div class="header">
+                <button onclick="openBar()" id="open-btn" class="mob-btn"><i class="fa fa-bars" aria-hidden="true"></i></button>
+                <button onclick="closeBar()" id="close-btn" class="mob-btn"><i class="fa fa-times" aria-hidden="true"></i></button>
+                <h3>Yogesh</h3>
+                <div class="nav" id="nav">
+                    <ul>
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="aboutus.html">About</a></li>
+                        <li><a href="courses.html">Page</a></li>
+                        <li><a href="downloads.html">Service &nbsp; <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                            <div class="submenu-1">
+                                <ul>
+                                    <li><a href="#">Mobile</a></li>
+                                    <li><a href="#">IOS</a></li>
+                                    <li><a href="#">Windows</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li><a href="#prices">Blog &nbsp;<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                            <div class="submenu-1">
+                                <ul>
+                                    <li><a href="#prices">Basic</a></li>
+                                    <li><a href="#prices">Advanced</a></li>
+                                    <li><a href="#prices">Premium</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li><a href="contactus.html">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div class="login-btn style-btn">
+                    <a href="signin.html"> <span>></span> Get a Quote</a>
+                </div>
+            </div>
+            <div class="heading">
+                <div class="heading-cont">
+                <div class="head-text hchild">
+                    <p>WE CREATE IDEAS, </p>
+                        <h1>Build Stunning <span class="bold">Website</span> With Slack Agency</h1>
+                        <p>Morbi sed lacus nec risus finibus feugiat et fermentum nibh. Pellentesque vitae ante at elit fringilla ac at purus.</p>
+                        <div class="explore-btn style-btn">
+                            <a href="courses.html"> <span> <i>></i> </span> Learn More</a>
+                        </div>
+                    </div>
+                    <div class="head-img hchild">
+                        <img src="pic1.png" alt="">
+                        <img src="pic2.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <a href="#" class="top-up"><i class="fa fa-angle-up" aria-hidden="true"></i></a>
+        </div>
+    </nav>
+</body>
+</html>
